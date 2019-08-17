40ee251d458a81e098aacb4be04fb57834682bf0
Ashish Kumar <ashishkumar.ashishkumar.58@gmail.com>
2019-08-17 15:33:45 +0530

    updating azure.tf

--- azure.tf
+++ azure.tf
@@ -1,3 +1,17 @@
-provider "azurerm" {
-    version = "=1.27.0"
+variable "subscription_id" {
 }
+variable "client_id" {}
+
+variable "client_secret" {}
+variable "tenant_id" {}
+
+provider "azurerm" {
+  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
+  version = "=1.27.0"
+
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
+}
+
